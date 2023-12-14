d3816c55a22271fbe078b367a2fbf92a43d0a0b6
TanyaIgnatenko <tanigna.work@gmail.com>
2023-12-14 22:00:54 +0700

    Add ability to edit chat title

--- src/components/ChatView.tsx
+++ src/components/ChatView.tsx
@@ -1,5 +1,8 @@
-import { useCallback } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import cn from 'classnames'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import {useClickAway} from 'react-use';
 
 import { Chat } from '../models/Chat';
 import { db } from '../models/db';
@@ -13,21 +16,44 @@ interface Props {
 };
 
 function ChatView({chat, isActive, onClick}: Props) {
+    const [chatTitle, setChatTitle] = useState(chat.title);
+    const [isEditMode, setIsEditMode] = useState(false);
     const handleClick = useCallback(() => {
         onClick(chat.id);
+    }, []);
+
+    const handleEdit = useCallback(() => {
+        setIsEditMode(true);
     }, []);
 
     const handleDelete = useCallback(() => {
         db.chats.delete(chat.id);
     }, []);
 
+    const chatButtonRef = useRef(null);
+    useClickAway(chatButtonRef, () => {
+        db.chats.update(chat.id, {title: chatTitle});
+        setIsEditMode(false);
+    });
+
     return (
         <button 
             onClick={handleClick} 
             className={cn('chat-button', {active: isActive})}
+            ref={chatButtonRef}
         >
-            {chat.title}
-            {isActive && (<button onClick={handleDelete} className="delete-button">x</button>)}
+            <input 
+                value={chatTitle} 
+                readOnly={!isEditMode}
+                onChange={(e) => setChatTitle(e.target.value)}
+                className="title-input"
+            />
+            {isActive && (
+                <div className="actions">
+                    <button onClick={handleEdit} className="edit-button"><EditIcon sx={{ color: '#676767', fontSize: 16 }} /></button>
+                    <button onClick={handleDelete} className="delete-button"><DeleteIcon sx={{ color: '#676767', fontSize: 16 }} /></button>
+                </div>
+            )}
         </button>
     );    
 }
