67b53044eb30e36a54e6e00f7de282f83255bff9
TanyaIgnatenko <tanigna.work@gmail.com>
2024-02-08 21:29:34 +0700

    Remove redundant initial messages

--- src/models/populate.ts
+++ src/models/populate.ts
@@ -11,20 +11,6 @@ export async function populate() {
         text: 'Enter your message to start a chat. This could be a question, a statement, a text and an image.'
       },
       role: 'assistant'
-    },
-    {
-      chatId,
-      content: {
-        text: 'What is the capital of Great Britain?'
-      },
-      role: 'user'
-    },
-    {
-      chatId,
-      content: {
-        text: 'London <3'
-      },
-      role: 'assistant'
     }
   ]);
 }
